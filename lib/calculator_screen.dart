7437a8507e1f36dedb53af7e74175749778ddb3c
Shahid-Zada <Shahidzada.cs@gmail.com>
2024-03-09 22:10:49 +0500

    a small unused directory warning

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -1,7 +1,5 @@
 import 'package:calculator_app/widgets.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:math_expressions/math_expressions.dart';
 
 class CalculatorScreen extends StatefulWidget {
